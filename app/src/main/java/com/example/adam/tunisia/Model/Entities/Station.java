d6a1470dd5ff72a0e19ed2532a5d71a4a012eab3
Adam <adam-bj@live.fr>
2016-03-29 13:37:49 +0100

    Testing Github integration

--- app/src/main/java/com/example/adam/tunisia/Model/Entities/Station.java
+++ app/src/main/java/com/example/adam/tunisia/Model/Entities/Station.java
@@ -4,60 +4,82 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class Station {
 
-    private String Name;
+    private int ROW_ID;
+    private String Nom;
+    private String Type;
     private LatLng LatLng ;
-    private Station Next;
-    private Station Previous;
+    private int Majeure;
+    private int SOC_ROW_ID;
 
     public Station() {
     }
 
-    public Station(String name, com.google.android.gms.maps.model.LatLng latLng) {
-        Name = name;
+    public Station(String nom, String type, com.google.android.gms.maps.model.LatLng latLng, int majeure, int SOC_ROW_ID) {
+        Nom = nom;
+        Type = type;
         LatLng = latLng;
-        Next = null;
-        Previous = null;
+        Majeure = majeure;
+        this.SOC_ROW_ID = SOC_ROW_ID;
     }
 
     @Override
     public String toString() {
         return "Station{" +
-                "Name='" + Name + '\'' +
-                ", Next=" + Next +
-                ", Previous=" + Previous +
+                "ROW_ID=" + ROW_ID +
+                ", Nom='" + Nom + '\'' +
+                ", Type='" + Type + '\'' +
+                ", LatLng=" + LatLng +
+                ", Majeure=" + Majeure +
+                ", SOC_ROW_ID=" + SOC_ROW_ID +
                 '}';
     }
 
-    public void setName(String name) {
-        Name = name;
+    public int getROW_ID() {
+        return ROW_ID;
     }
 
-    public void setNext(Station next) {
-        Next = next;
+    public String getNom() {
+        return Nom;
     }
 
-    public void setPrevious(Station previous) {
-        Previous = previous;
-    }
-
-    public void setLatLng( com.google.android.gms.maps.model.LatLng latLng) {
-        LatLng = latLng;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public Station getNext() {
-        return Next;
-    }
-
-    public Station getPrevious() {
-        return Previous;
+    public String getType() {
+        return Type;
     }
 
     public com.google.android.gms.maps.model.LatLng getLatLng() {
         return LatLng;
+    }
+
+    public int getMajeure() {
+        return Majeure;
+    }
+
+    public int getSOC_ROW_ID() {
+        return SOC_ROW_ID;
+    }
+
+    public void setROW_ID(int ROW_ID) {
+        this.ROW_ID = ROW_ID;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public void setLatLng(com.google.android.gms.maps.model.LatLng latLng) {
+        LatLng = latLng;
+    }
+
+    public void setMajeure(int majeure) {
+        Majeure = majeure;
+    }
+
+    public void setSOC_ROW_ID(int SOC_ROW_ID) {
+        this.SOC_ROW_ID = SOC_ROW_ID;
     }
 
 }
