2c2bd7e0262fb50b39635315b14bafc4c1a75da7
Adam <adam-bj@live.fr>
2016-04-01 19:26:00 +0100

    TEST

---
+++ app/src/main/java/com/example/adam/tunisia/Model/Rest/Interfaces/ISociete.java
@@ -0,0 +1,15 @@
+package com.example.adam.tunisia.Model.Rest.Interfaces;
+
+import com.example.adam.tunisia.Model.Entities.Societe;
+
+import java.util.List;
+
+//import retrofit2.Call;
+//import retrofit2.http.GET;
+
+public interface ISociete {
+
+ // @GET("rest/societes/crud")
+  //Call<List<Societe>> getWheatherReport();
+
+}
---
+++ app/src/main/java/com/example/adam/tunisia/Model/Rest/RetrofitFeedback.java
@@ -0,0 +1,19 @@
+package com.example.adam.tunisia.Model.Rest;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.adam.tunisia.Model.Entities.Feedback;
+//import com.google.gson.Gson;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class RetrofitFeedback {
+
+
+
+}
